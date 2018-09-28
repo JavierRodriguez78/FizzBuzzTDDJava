f77e6de2b3565e02194987b37207e6673200f6e3
Xavi <xavi@geekshubs.com>
2018-09-28 12:21:38 +0200

    [Green]Create FizzBuzzClass and FizzBuzzTest

---
+++ src/test/java/com/geekshubsacademy/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,14 @@
+package com.geekshubsacademy.fizzbuzz;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class FizzBuzzTest {
+
+    @Test
+    public void firstTest()
+    {
+        assertTrue(true);
+    }
+}
